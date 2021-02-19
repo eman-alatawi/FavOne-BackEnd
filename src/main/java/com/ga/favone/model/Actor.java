f36271af9ff87c8e5033ae3e09aa61b39d028168
eman-e-alatawi <eman.eid.alatawi@gmail.com>
2021-02-19 19:41:36 +0300

    add Actor model

---
+++ src/main/java/com/ga/favone/model/Actor.java
@@ -0,0 +1,115 @@
+package com.ga.favone.model;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
+
+import javax.persistence.*;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "Actor")
+public class Actor {
+
+	@Id
+	@GeneratedValue
+	private int id;
+
+	private String fullName;
+
+	private Date dateOfBirth;
+
+	private String gender;
+
+	@Column(nullable = false, updatable = true, length = 5000)
+	private String biography;
+
+	@Column(nullable = false, updatable = true, length = 2000)
+	private String picture;
+
+	private String socialAccount;
+
+	@Column(name = "createdAt", nullable = false, updatable = false)
+	@CreationTimestamp
+	private LocalDateTime createAt;
+
+	@Column(name = "updatedAt", nullable = false, updatable = true)
+	@UpdateTimestamp
+	private LocalDateTime updateAt;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getBiography() {
+		return biography;
+	}
+
+	public void setBiography(String biography) {
+		this.biography = biography;
+	}
+
+	public String getPicture() {
+		return picture;
+	}
+
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+
+	public String getSocialAccount() {
+		return socialAccount;
+	}
+
+	public void setSocialAccount(String socialAccount) {
+		this.socialAccount = socialAccount;
+	}
+
+	public LocalDateTime getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(LocalDateTime createAt) {
+		this.createAt = createAt;
+	}
+
+	public LocalDateTime getUpdateAt() {
+		return updateAt;
+	}
+
+	public void setUpdateAt(LocalDateTime updateAt) {
+		this.updateAt = updateAt;
+	}
+	
+	
+
+}
